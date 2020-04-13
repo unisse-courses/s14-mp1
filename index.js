5de220131ed86e9a43460a95d1fc6e4383e184e7
Jolson Cruz <56365930+jolsoncruz@users.noreply.github.com>
2020-04-13 21:04:52 +0800

    Linked to DB

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
-var express = require('express');
-var sassMiddleware = require('node-sass-middleware');
-var path = require('path');
-var hbs = require('express-handlebars');
-var app = express();
+const mongodb = require('mongodb');
+const express = require('express');
+const sassMiddleware = require('node-sass-middleware');
+const path = require('path');
+const hbs = require('express-handlebars');
+const app = express();
+
+const mongoClient = mongodb.MongoClient;
+const databaseURL = "mongodb+srv://admin:admin@commforumdb-df0ls.mongodb.net/test";
+const dbname = "commforumdb";
+
+const options = { useUnifiedTopology: true };
 
 app.set('view engine', 'hbs');
 
 app.engine('hbs',hbs({
 	extname: 'hbs',
+	helpers: {
+        ifCond: function(v1, v2, options) {
+		  if(v1 === v2) {
+		    return options.fn(this);
+		  }
+		  return options.inverse(this);
+		}
+    },
 	defaultView: 'main',
 	layoutsDir: __dirname + '/views/layouts/',
 	partialsDir: __dirname + '/views/partials/'
@@ -27,6 +42,9 @@ app.use(sassMiddleware({
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res){
+	console.log(Math.round(Math.random() * 35));
+	console.log(Math.round(Math.random() * 35));
+	console.log(Math.round(Math.random() * 35));
 	res.render('home',{
 		title: 'Home',
 		jumbotronCustom: "background-image: linear-gradient(to bottom, rgba(255,255,255,0.6) 0%,rgba(255,255,255,0.9) 100%), url('/assets/home_header.jpg')",
@@ -50,6 +68,7 @@ app.get('/', function(req, res){
 		p3Link: '/'
 	});
 });
+
 
 app.get('/colleges', function(req, res){
 	res.render('colleges',{
@@ -98,8 +117,33 @@ app.get('/colleges/cla', function(req, res){
 		jumbotronHeader: 'College of Liberal Arts',
 		jumbotronMessage: "The home of tomorrow's world leaders",
 		jumbotronBtn: 'View CLA Professors',
-		jumbotronLink: 'colleges/cla/professors'
+		jumbotronLink: 'cla/professors'
 	});
+});
+
+app.get('/colleges/cla/professors', function(req, res){
+	mongoClient.connect(databaseURL, options, function(err, client) {
+	    if(err) throw err;
+	    // Connect to the same database
+	    const dbo = client.db(dbname);
+
+	    dbo.collection("professor").find({college: "CLA"}).toArray(function(err, result) {
+	      if(err) throw err;
+	  
+	      console.log("Read Successful!");
+	      client.close();
+	      res.render('professors', {
+	      	special: true,
+	      	title: 'CLA Professors',
+			jumbotronCustom: "background-image: linear-gradient(to bottom, rgba(255,255,255,0.6) 0%,rgba(255,255,255,0.9) 100%), url('/assets/college_header.jpg')",
+			jumbotronHeader: 'Professors',
+			jumbotronMessage: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut perspiciatis maxime veniam facere, libero ducimus in nostrum. Nam quam aliquam eos amet, error enim iste a facilis minima voluptatum quo!',
+			jumbotronLink: '/',
+			jumbotronBtn: 'Back to Homepage',
+	        professor: result,
+	      });
+	    });
+  	});
 });
 
 app.get('/colleges/cob', function(req, res){
@@ -109,8 +153,33 @@ app.get('/colleges/cob', function(req, res){
 		jumbotronHeader: 'College of Business',
 		jumbotronMessage: "The home of tomorrow's world leaders",
 		jumbotronBtn: 'View COB Professors',
-		jumbotronLink: 'colleges/cob/professors'
+		jumbotronLink: 'cob/professors'
 	});
+});
+
+app.get('/colleges/cob/professors', function(req, res){
+	mongoClient.connect(databaseURL, options, function(err, client) {
+	    if(err) throw err;
+	    // Connect to the same database
+	    const dbo = client.db(dbname);
+
+	    dbo.collection("professor").find({college: "COB"}).toArray(function(err, result) {
+	      if(err) throw err;
+	  
+	      console.log("Read Successful!");
+	      client.close();
+	      res.render('professors', {
+	      	special: true,
+	      	title: 'COB Professors',
+			jumbotronCustom: "background-image: linear-gradient(to bottom, rgba(255,255,255,0.6) 0%,rgba(255,255,255,0.9) 100%), url('/assets/college_header.jpg')",
+			jumbotronHeader: 'Professors',
+			jumbotronMessage: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut perspiciatis maxime veniam facere, libero ducimus in nostrum. Nam quam aliquam eos amet, error enim iste a facilis minima voluptatum quo!',
+			jumbotronLink: '/',
+			jumbotronBtn: 'Back to Homepage',
+	        professor: result,
+	      });
+	    });
+  	});
 });
 
 app.get('/colleges/coe', function(req, res){
@@ -120,8 +189,33 @@ app.get('/colleges/coe', function(req, res){
 		jumbotronHeader: 'College of Engineering',
 		jumbotronMessage: "The home of tomorrow's world leaders",
 		jumbotronBtn: 'View COE Professors',
-		jumbotronLink: 'colleges/coe/professors'
+		jumbotronLink: 'coe/professors'
 	});
+});
+
+app.get('/colleges/coe/professors', function(req, res){
+	mongoClient.connect(databaseURL, options, function(err, client) {
+	    if(err) throw err;
+	    // Connect to the same database
+	    const dbo = client.db(dbname);
+
+	    dbo.collection("professor").find({college: "COE"}).toArray(function(err, result) {
+	      if(err) throw err;
+	  
+	      console.log("Read Successful!");
+	      client.close();
+	      res.render('professors', {
+	      	special: true,
+	      	title: 'COE Professors',
+			jumbotronCustom: "background-image: linear-gradient(to bottom, rgba(255,255,255,0.6) 0%,rgba(255,255,255,0.9) 100%), url('/assets/college_header.jpg')",
+			jumbotronHeader: 'Professors',
+			jumbotronMessage: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut perspiciatis maxime veniam facere, libero ducimus in nostrum. Nam quam aliquam eos amet, error enim iste a facilis minima voluptatum quo!',
+			jumbotronLink: '/',
+			jumbotronBtn: 'Back to Homepage',
+	        professor: result,
+	      });
+	    });
+  	});
 });
 
 app.get('/colleges/ced', function(req, res){
@@ -131,8 +225,33 @@ app.get('/colleges/ced', function(req, res){
 		jumbotronHeader: 'College of Education',
 		jumbotronMessage: "The home of tomorrow's world leaders",
 		jumbotronBtn: 'View CED Professors',
-		jumbotronLink: 'colleges/ced/professors'
+		jumbotronLink: 'ced/professors'
 	});
+});
+
+app.get('/colleges/ced/professors', function(req, res){
+	mongoClient.connect(databaseURL, options, function(err, client) {
+	    if(err) throw err;
+	    // Connect to the same database
+	    const dbo = client.db(dbname);
+
+	    dbo.collection("professor").find({college: "CED"}).toArray(function(err, result) {
+	      if(err) throw err;
+	  
+	      console.log("Read Successful!");
+	      client.close();
+	      res.render('professors', {
+	      	special: true,
+	      	title: 'CED Professors',
+			jumbotronCustom: "background-image: linear-gradient(to bottom, rgba(255,255,255,0.6) 0%,rgba(255,255,255,0.9) 100%), url('/assets/college_header.jpg')",
+			jumbotronHeader: 'Professors',
+			jumbotronMessage: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut perspiciatis maxime veniam facere, libero ducimus in nostrum. Nam quam aliquam eos amet, error enim iste a facilis minima voluptatum quo!',
+			jumbotronLink: '/',
+			jumbotronBtn: 'Back to Homepage',
+	        professor: result,
+	      });
+	    });
+  	});
 });
 
 app.get('/colleges/ccs', function(req, res){
@@ -142,8 +261,33 @@ app.get('/colleges/ccs', function(req, res){
 		jumbotronHeader: 'College of Computer Studies',
 		jumbotronMessage: "The home of tomorrow's world leaders",
 		jumbotronBtn: 'View CCS Professors',
-		jumbotronLink: 'colleges/ccs/professors'
+		jumbotronLink: 'ccs/professors'
 	});
+});
+
+app.get('/colleges/ccs/professors', function(req, res){
+	mongoClient.connect(databaseURL, options, function(err, client) {
+	    if(err) throw err;
+	    // Connect to the same database
+	    const dbo = client.db(dbname);
+
+	    dbo.collection("professor").find({college: "CCS"}).toArray(function(err, result) {
+	      if(err) throw err;
+	  
+	      console.log("Read Successful!");
+	      client.close();
+	      res.render('professors', {
+	      	special: true,
+	      	title: 'CCS Professors',
+			jumbotronCustom: "background-image: linear-gradient(to bottom, rgba(255,255,255,0.6) 0%,rgba(255,255,255,0.9) 100%), url('/assets/college_header.jpg')",
+			jumbotronHeader: 'Professors',
+			jumbotronMessage: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut perspiciatis maxime veniam facere, libero ducimus in nostrum. Nam quam aliquam eos amet, error enim iste a facilis minima voluptatum quo!',
+			jumbotronLink: '/',
+			jumbotronBtn: 'Back to Homepage',
+	        professor: result,
+	      });
+	    });
+  	});
 });
 
 app.get('/colleges/cos', function(req, res){
@@ -153,8 +297,33 @@ app.get('/colleges/cos', function(req, res){
 		jumbotronHeader: 'College of Science',
 		jumbotronMessage: "The home of tomorrow's world leaders",
 		jumbotronBtn: 'View COS Professors',
-		jumbotronLink: 'colleges/cos/professors'
+		jumbotronLink: 'cos/professors'
 	});
+});
+
+app.get('/colleges/cos/professors', function(req, res){
+	mongoClient.connect(databaseURL, options, function(err, client) {
+	    if(err) throw err;
+	    // Connect to the same database
+	    const dbo = client.db(dbname);
+
+	    dbo.collection("professor").find({college: "COS"}).toArray(function(err, result) {
+	      if(err) throw err;
+	  
+	      console.log("Read Successful!");
+	      client.close();
+	      res.render('professors', {
+	      	special: true,
+	      	title: 'COS Professors',
+			jumbotronCustom: "background-image: linear-gradient(to bottom, rgba(255,255,255,0.6) 0%,rgba(255,255,255,0.9) 100%), url('/assets/college_header.jpg')",
+			jumbotronHeader: 'Professors',
+			jumbotronMessage: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut perspiciatis maxime veniam facere, libero ducimus in nostrum. Nam quam aliquam eos amet, error enim iste a facilis minima voluptatum quo!',
+			jumbotronLink: '/',
+			jumbotronBtn: 'Back to Homepage',
+	        professor: result,
+	      });
+	    });
+  	});
 });
 
 app.get('/colleges/soe', function(req, res){
@@ -164,8 +333,58 @@ app.get('/colleges/soe', function(req, res){
 		jumbotronHeader: 'School of Economics',
 		jumbotronMessage: "The home of tomorrow's world leaders",
 		jumbotronBtn: 'View SOE Professors',
-		jumbotronLink: 'colleges/soe/professors'
+		jumbotronLink: 'soe/professors'
 	});
+});
+
+app.get('/colleges/soe/professors', function(req, res){
+	mongoClient.connect(databaseURL, options, function(err, client) {
+	    if(err) throw err;
+	    // Connect to the same database
+	    const dbo = client.db(dbname);
+
+	    dbo.collection("professor").find({college: "SOE"}).toArray(function(err, result) {
+	      if(err) throw err;
+	  
+	      console.log("Read Successful!");
+	      client.close();
+	      res.render('professors', {
+	      	special: true,
+	      	title: 'SOE Professors',
+			jumbotronCustom: "background-image: linear-gradient(to bottom, rgba(255,255,255,0.6) 0%,rgba(255,255,255,0.9) 100%), url('/assets/college_header.jpg')",
+			jumbotronHeader: 'Professors',
+			jumbotronMessage: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut perspiciatis maxime veniam facere, libero ducimus in nostrum. Nam quam aliquam eos amet, error enim iste a facilis minima voluptatum quo!',
+			jumbotronLink: '/',
+			jumbotronBtn: 'Back to Homepage',
+	        professor: result,
+	      });
+	    });
+  	});
+});
+
+app.get('/professors', function(req, res){
+	mongoClient.connect(databaseURL, options, function(err, client) {
+	    if(err) throw err;
+	    // Connect to the same database
+	    const dbo = client.db(dbname);
+
+	    dbo.collection("professor").find({}).toArray(function(err, result) {
+	      if(err) throw err;
+	  
+	      console.log("Read Successful!");
+	      client.close();
+	      res.render('professors', {
+	      	title: 'Professors',
+			jumbotronCustom: "background-image: linear-gradient(to bottom, rgba(255,255,255,0.6) 0%,rgba(255,255,255,0.9) 100%), url('/assets/college_header.jpg')",
+			jumbotronHeader: 'Professors',
+			jumbotronMessage: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut perspiciatis maxime veniam facere, libero ducimus in nostrum. Nam quam aliquam eos amet, error enim iste a facilis minima voluptatum quo!',
+			jumbotronLink: '/',
+			jumbotronBtn: 'Back to Homepage',
+	        professor: result,
+	      });
+	    });
+	    // More stuff to go here ...
+  });
 });
 
 app.listen(app.get('port'), function(){
