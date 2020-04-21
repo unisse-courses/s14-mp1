1ac2fda507c8f7ec53faeaa2a13ca99445468445
Jolson Cruz <56365930+jolsoncruz@users.noreply.github.com>
2020-04-22 02:18:45 +0800

    Create comment.js

---
+++ models/comment.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const Int32 = require('mongoose-int32');
+const ObjectId = Schema.Types.ObjectId;
+
+const databaseURL = 'mongodb+srv://admin:admin@commforumdb-df0ls.mongodb.net/commforumdb';
+
+const options = {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	useFindAndModify: false
+};
+
+mongoose.connect(databaseURL, options);
+
+var commentSchema = new Schema({
+	reviewRef: {type: ObjectId, ref: 'professor', required: true},
+	studentRef: {type: ObjectId, ref: 'user', required: true},
+	commentContent: {type: String, required: true},
+});
+
+module.exports = mongoose.model('comment', commentSchema);
