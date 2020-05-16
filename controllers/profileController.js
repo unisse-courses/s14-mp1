55e4c6d353a4c6847e67f58499c311992729fc19
Ryakeee <61469562+Ryakeee@users.noreply.github.com>
2020-05-16 17:30:34 +0800

    updated

---
+++ controllers/profileController.js
@@ -0,0 +1,26 @@
+//Import Models
+const commentModel = require('../models/comment');
+const reviewModel = require('../models/review');
+
+exports.loadProfile = function(req, res){
+	if (req.session.loggedin){
+		reviewModel.getAll({studentId: req.session.idNum}, function(reviews){
+			commentModel.getAllProfile({studentRef: req.session.studentRef}, function(comments){
+				res.render('frontend/profile',{
+					session: req.session,
+					reviews: reviews,
+					comments: comments,
+					title: 'Profile',
+					session: req.session,
+					jumbotronImage: '/assets/headers/user_header.jpg',
+					jumbotronHeader: 'Hello ' + req.session.nickname + ',',
+					jumbotronMessage: "This page shows your most recent contribution to the DLSU Community Forum. You may also change your password through the form below.",
+					jumbotronBtn: 'Back to Homepage',
+					jumbotronLink: '/'
+				});
+			});
+		});
+	} else{
+		res.redirect('/login');
+	}
+};
